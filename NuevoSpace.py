bc93411b813cbf199b86c500e73df9e970a88658
UlisesRodriguezPerez <uli.rp1999@gmail.com>
2020-08-05 19:12:27 -0600

    Ajustes en el juego.

--- NuevoSpace.py
+++ NuevoSpace.py
@@ -1,8 +1,16 @@
 import math
 import random
-
 import pygame
 from pygame import mixer
+
+#   Constantes.
+PUNTAJE_NUEVA_VIDA = 20                 #   Cantidad de puntos para obtener una vida.
+NUM_OF_ENEMIES = 5                      #   Cantidad de enemigos al inicio.
+NUM_OF_ENEMIES_LEVES = NUM_OF_ENEMIES   #   
+LIFES = 2                               #   Vidas al inicio del juego.
+PUNTOS_IMG_ENEMY_0 = 1                  #   Puntos por desruir enemigo de la imagen enemy0.png.
+PUNTOS_IMG_ENEMY_1 = 2                  #   Puntos por destruir enemigo de la imagen enemy1.png.
+PUNTOS_IMG_ENEMY_2 = 3                  #   Puntos por destruir enemigo de la imagen enemy2.png.
 
     #Clase Game( principal).
 class Game:
@@ -35,10 +43,12 @@ class Game:
             #   Acción de las teclas.
             self.event_type()
             #   validar campos del player.
-            self.player_validation()
+            self.player.player_validation()
             #   Enemy Movement.
             self.enemy_Movement()
             #   Bullet Movement.
+            self.bullet_movement()
+            #   Bullet enemigo.
             self.bullet_movement()
             #   Actualizar jugador.
             self.player.set_player(self,self.player.playerX, self.player.playerY)
@@ -73,6 +83,7 @@ class Game:
         self.game_over = GameOver()
         self.level_of_game = Level()
         self.lifes = Life()
+        self.bullet_enemigo = BulletEnemigo()
 
     #   Función para ver qué accion debe hacer el programa según la tecla presionada.
     def event_type(self):
@@ -81,6 +92,10 @@ class Game:
                 self.running = False
             # if keystroke is pressed check whether its right or left
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.restar()
+                    pygame.quit()
+                    return main()   #REVISAR Para que reinicie el jeugo con la tecla esc.
                 if event.key == pygame.K_LEFT:
                     self.player.playerX_change = -5
                 if event.key == pygame.K_RIGHT:
@@ -96,19 +111,19 @@ class Game:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     self.player.playerX_change = 0
 
-    #   Valida los campos del player, para que no se salga de la pantalla.
-    def player_validation(self):
-        self.player.playerX += self.player.playerX_change
-        if self.player.playerX <= 0:
-            self.player.playerX = 0
-        elif self.player.playerX >= 736:
-            self.player.playerX = 736
+    def restar(self):
+        self.enemy.enemyImg = []
+        self.enemy.enemyX = []
+        self.enemy.enemyY = []
+        self.enemy.enemyX_change = []
+        self.enemy.enemyY_change = []
+        self.enemy.enemy_name_Img = []
 
     #   Todo el movimiento del enemigo.
     def enemy_Movement(self):
-        #print(self.enemy.num_of_enemies)
+        
         for i in range(self.enemy.num_of_enemies):
-            # Game Over
+            self.disparo_enemigo(i)
             if self.enemy.enemyY[i] > 440:
                 if self.lifes.lifes_of_level != 1:
                     self.lifes.lifes_of_level = self.lifes.lifes_of_level - 1
@@ -118,10 +133,10 @@ class Game:
                     for j in range(self.enemy.num_of_enemies):
                         self.enemy.enemyY[j] = 2000
                     self.game_over.game_over_text(self)
-                    break
-            self.enemy.enemyX[i] += self.enemy.enemyX_change[i]
+            self.enemy.enemyX[i] #+= self.enemy.enemyX_change[i]
             #   Enemy movement
-            self.enemy_direction(i)
+            #self.enemy_direction(i)
+            self.enemy.enemyY[i] += 0.2
             # Collision
             collision = self.bullet.isCollision(self.enemy.enemyX[i], self.enemy.enemyY[i], self.bullet.bulletX, self.bullet.bulletY)
             if collision:
@@ -134,6 +149,16 @@ class Game:
             else:
                 self.enemy.enemy(self,self.enemy.enemyX[i], self.enemy.enemyY[i], i)
              
+    #   Función encargada de generar que el enemigo dispare.
+    def disparo_enemigo(self,i):
+        randomm = 0
+        randomm = random.randint(0, 1000)
+        if randomm > 50:
+            self.bullet_enemigo.bullet_enemyX = self.enemy.enemyX[i]
+            self.bullet_enemigo.bullet_enemyY = self.enemy.enemyY[i]
+            self.bullet_enemigo.fire_bullet_enemy(self,self.bullet_enemigo.bullet_enemyX, self.bullet_enemigo.bullet_enemyY)
+            
+
 
     # Bullet Movement.
     def bullet_movement(self):
@@ -154,23 +179,27 @@ class Game:
         self.bullet.bullet_state = "ready"
         suma = self.clasificar_enemy(i)
         self.score.score_value += suma
+        self.score.score_life += suma
         #   Función que se encarga de destruir el enemigo y crear la explosión.
         self.destruir_enemigo(i)
-        puntaje = self.score.score_value
-        if puntaje % 10 == 0:           #   Obtiene una vida cada que sume 10 puntos.
+        puntaje = self.score.score_life
+        if puntaje >= PUNTAJE_NUEVA_VIDA:           #   Obtiene una vida cada que sume 10 puntos.
             self.lifes.lifes_of_level += 1
+            self.score.score_life = 0
         if self.enemy.num_of_enemies == 0:
             #   pasa al siguiente nivel.
             self.siguiente_nivel()
             self.level_of_game.level += 1
+
     def clasificar_enemy(self,i):
+        
         #REVISAR pendiente ver cómo commparar imagenes.
-        if self.enemy.enemyImg[i] == pygame.image.load('enemy0.png'):
-            return 1
-        elif self.enemy.enemyImg[i] == pygame.image.load('enemy1.png'):
-            return 2
+        if self.enemy.enemy_name_Img[i] == 'enemy0.png':
+            return PUNTOS_IMG_ENEMY_0
+        elif self.enemy.enemy_name_Img[i] == 'enemy1.png':
+            return PUNTOS_IMG_ENEMY_1
         else:
-            return 1
+            return PUNTOS_IMG_ENEMY_2
 
     def destruir_enemigo(self,i):
         self.enemy.enemyImg[i] = pygame.image.load('explosion.png')
@@ -180,19 +209,17 @@ class Game:
         self.enemy.enemyX_change.pop(i)
         self.enemy.enemyY_change.pop(i)
         self.enemy.enemyImg.pop(i)
+        self.enemy.enemy_name_Img.pop(i)
         self.enemy.num_of_enemies = self.enemy.num_of_enemies - 1
 
     #   Función encargada de aumentar los enemigos cuando se acaben.
     def siguiente_nivel(self):
-        
         #   Carga nuevamente los enemigos.
         self.enemy.num_of_enemies_leves = self.enemy.num_of_enemies_leves + 1
         self.enemy.num_of_enemies = self.enemy.num_of_enemies_leves
         for i in range(self.enemy.num_of_enemies_leves):
             self.enemy.generar_enemy()
             # self.enemy.enemy(self,self.enemy.enemyX[i], self.enemy.enemyY[i], i)
-        
-        
         return self.enemy_Movement()
 
     #   Función para cambiar de dirección el enemigo.
@@ -216,6 +243,14 @@ class Player:
     #   Función para actualizar el player.
     def set_player(self,game,x, y):
         game.screen.blit(self.playerImg, (x, y))
+
+    #   Valida los campos del player, para que no se salga de la pantalla.
+    def player_validation(self):
+        self.playerX += self.playerX_change
+        if self.playerX <= 0:
+            self.playerX = 0
+        elif self.playerX >= 736:
+            self.playerX = 736
 
     #   Clase Bullet.
 class Bullet: # Bullet
@@ -248,6 +283,36 @@ class Bullet: # Bullet
         else:
             return False
 
+class BulletEnemigo:
+    def __init__(self):
+        self.bulletImg = pygame.image.load('bullet.png')
+        self.bullet_enemyX = 0
+        self.bullet_enemyY = 0
+        self.bullet_enemyX_change = 0
+        self.bullet_enemyY_change = 10
+        self.bullet_enemy_state = "ready"
+        self.bullet_enemy_range = 16
+    #   Cambia el estado a disparar.
+    def fire_bullet_enemy(self,game,x, y):
+        self.bullet_enemy_state = "fire"
+        game.screen.blit(self.bulletImg, (x + self.bullet_enemy_range, y - 10)) 
+
+     #   Función booleana para saber si la bala colicionó.
+    def isCollision(self,enemyX,enemyY,bulletX,bulletY):
+        distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+        if distance < 27:
+            return True
+        else:
+            return False
+    # Bullet enemy Movement.
+    def bullet_movement(self):
+        if self.bullet_enemyY <= 0:
+            self.bullet_enemyY = 480
+            self.bullet_enemy_state = "ready"
+
+        if self.bullet_enemy_state == "fire":
+            self.fire_bullet_enemy(self,self.bullet_enemyX, self.bullet_enemyY)
+            self.bullet_enemyY -= self.bullet_enemyY_change
 
     #Clase Enemy.
 class Enemy: 
@@ -257,8 +322,9 @@ class Enemy:
     enemyY = []
     enemyX_change = []
     enemyY_change = []
-    num_of_enemies = 3
-    num_of_enemies_leves = 3
+    enemy_name_Img = []
+    num_of_enemies = NUM_OF_ENEMIES 
+    num_of_enemies_leves = NUM_OF_ENEMIES_LEVES
 
     #   Constructor de la clase.
     def __init__(self):
@@ -268,6 +334,7 @@ class Enemy:
     def generar_enemy(self):
         enemy_random = random.randint(0, 2)
         self.enemyImg.append(pygame.image.load('enemy'+ str(enemy_random)+'.png'))
+        self.enemy_name_Img.append('enemy'+ str(enemy_random)+'.png')
         self.enemyX.append(random.randint(0, 736))
         self.enemyY.append(random.randint(50, 150))
         self.enemyX_change.append(4)
@@ -280,7 +347,7 @@ class Enemy:
 class Life:
     #   Constructor de la clase.
     def __init__(self):
-        self.lifes_of_level = 3
+        self.lifes_of_level = LIFES
         self.textX = 200
         self.textY = 10
         self.font = pygame.font.Font('freesansbold.ttf', 32)
@@ -309,6 +376,7 @@ class Score:
     #   Constructor de la clase.
     def __init__(self):
         self.score_value = 0
+        self.score_life = 0
         self.textX = 10
         self.testY = 10
         self.font = pygame.font.Font('freesansbold.ttf', 32)
@@ -328,6 +396,14 @@ class GameOver:
     def game_over_text(self,game):
         over_text = self.over_font.render("GAME OVER", True, (255, 255, 255))
         game.screen.blit(over_text, (200, 250))
+
+    #   Función para reiniciar el programa.
+    # def restar_game(self):
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.KEYDOWN:
+                
+
 #   main del programa.
-if __name__ == '__main__':
+def main():
     game = Game(800, 600)
+main()
